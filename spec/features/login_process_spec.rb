2b98fa59791d99235e5f02c3b0de0d061e7ebf0e
keshav-c <keshav1985@gmail.com>
2020-02-29 15:14:31 +0530

    Security: puma update

--- spec/features/login_process_spec.rb
+++ spec/features/login_process_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature "LoginProcesses", type: :feature do
+RSpec.feature 'LoginProcesses', type: :feature do
   describe 'visit homepage' do
     it 'if not logged in redirects to login page' do
       visit root_url
-      expect(page).to have_title("Log in | TechPodium")
+      expect(page).to have_title('Log in | TechPodium')
     end
 
-    it 'if logged in, shows the user\'s home page' 
+    it 'if logged in, shows the user\'s home page'
   end
 end
