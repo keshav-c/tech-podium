a5e0d6e834751f9b6757eda3d78247acddb26ebf
keshav-c <keshav1985@gmail.com>
2020-03-01 22:21:54 +0530

    Prepare for relationships migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
             length: { minimum: 4, maximum: 50 }
 
   has_many :messages
+  has_many :following_relationships,
+           class_name: 'Relationship',
+           foreign_key: 'follower_id',
+           dependent: :destroy
+  has_many :following,
+           through: :following_relationships,
+           source: :followed
 end
