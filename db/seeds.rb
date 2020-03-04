593ae5b13ca40135a714b2aec15c13da4a044b4e
keshav-c <keshav1985@gmail.com>
2020-03-04 06:21:35 +0530

    Database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..30).each { |i| User.create(username: "user-#{i}", fullname: "The User #{i}") }
+u1 = User.first
+(11..20).each { |i| u1.follow(User.find(i)) }
+User.all.each do |u|
+  2.times { |i| u.messages.create(text: "Message number #{i + 1} by #{u.fullname}") }
+end
+(11..15).each do |i|
+  u = User.find(i)
+  u.messages.each { |m| u1.like_message(m) }
+end
