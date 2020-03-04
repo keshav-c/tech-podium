46e004a25cf1e1befdd3cc819b67c29a7e0d32cb
keshav-c <keshav1985@gmail.com>
2020-03-04 11:14:08 +0530

    Clean up SQL

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,20 +8,16 @@ class Message < ApplicationRecord
   default_scope { order(created_at: :desc) }
 
   def self.feed(user, current_user, user_logged_in:)
-    current_user_liked = 
-      "CASE 
-       WHEN #{current_user.id} IN (SELECT user_id FROM likes WHERE likes.message_id = messages.id)
-       THEN TRUE 
-       ELSE FALSE 
-       END 
-       AS liked"
+    users_liked = 'SELECT user_id FROM likes WHERE likes.message_id = messages.id'
+    condition = "#{current_user.id} IN (#{users_liked})"
+    current_user_liked = "CASE WHEN #{condition} THEN TRUE ELSE FALSE END AS liked"
     if user_logged_in
       following_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
-      Message.
-        where("user_id IN (#{following_ids}) OR user_id = :user_id",
-              user_id: user.id).
-        includes(:user).
-        select("messages.*, #{current_user_liked}")
+      Message
+        .where("user_id IN (#{following_ids}) OR user_id = :user_id",
+               user_id: user.id)
+        .includes(:user)
+        .select("messages.*, #{current_user_liked}")
     else
       user.messages.includes(:user).select("messages.*, #{current_user_liked}")
     end
